01d5867a37810d0b0ae47f8cc8794e00cd86ff6a
Dhruv <chodvadiyadhruv06@gmail.com>
2024-06-04 22:35:41 +0530

    add changes

--- HotalService/src/main/java/com/lcwd/hotel/HotalService/controllers/HotelController.java
+++ HotalService/src/main/java/com/lcwd/hotel/HotalService/controllers/HotelController.java
@@ -31,9 +31,5 @@ public class HotelController {
     }
 
     //all hotel get
-    @GetMapping
-    public ResponseEntity<List<Hotel>> getAllHotel(){
-        List<Hotel> allHotel = hotelService.getAll();
-        return ResponseEntity.ok(allHotel);
-    }
+
 }
